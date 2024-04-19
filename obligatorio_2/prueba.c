b5fe1329dc56e5a2d539246bdb922f3c8438a3ad
ZZAZZ99 <nicolasryzhov@gmail.com>
2024-04-19 18:30:47 +0200

    "arreglado"

--- obligatorio_2/prueba.c
+++ obligatorio_2/prueba.c
@@ -5,88 +5,68 @@
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_randist.h>
 
-short int petizo_gsl(short int dim);
-double real_gsl();
-int entero_gsl();
+short int petizo_gsl(gsl_rng * r, short int dim);
+double real_gsl(gsl_rng * r);
+int entero_gsl(gsl_rng * r);
 
 int main ()
 {
     int i;
+    const gsl_rng_type * T;
+    gsl_rng * r;
+
+    gsl_rng_env_setup();
+
+    T = gsl_rng_default;
+    r = gsl_rng_alloc (T);
 
     printf("100 random short int numbers between 0 and dim:\n");
     for (i = 0; i < 100; i++) {
-        short int random_short = petizo_gsl(100);
+        short int random_short = petizo_gsl(r, 100);
         printf("%d ", random_short);
     }
     printf("\n");
 
     printf("100 random double numbers between 0 and 1:\n");
     for (i = 0; i < 100; i++) {
-        double random_number = real_gsl();
+        double random_number = real_gsl(r);
         printf("%f ", random_number);
     }
     printf("\n");
 
     printf("100 random integers:\n");
     for (i = 0; i < 100; i++) {
-        int random_int = entero_gsl();
+        int random_int = entero_gsl(r);
         printf("%d ", random_int);
     }
     printf("\n");
 
+    gsl_rng_free(r);
+
     return 0;
 }
 
-short int petizo_gsl(short int dim) 
+short int petizo_gsl(gsl_rng * r, short int dim) 
 {
-    const gsl_rng_type * T;
-    gsl_rng * r;
-
-    gsl_rng_env_setup();
-
-    T = gsl_rng_default;
-    r = gsl_rng_alloc (T);
-
     // Generar número aleatorio short int entre 0 y dim
     short int random_short = gsl_rng_uniform_int(r, dim);
-
-    gsl_rng_free(r);
 
     return random_short;
 }
 
-double real_gsl() 
+double real_gsl(gsl_rng * r) 
 {
-    const gsl_rng_type * T;
-    gsl_rng * r;
-
-    gsl_rng_env_setup();
-
-    T = gsl_rng_default;
-    r = gsl_rng_alloc (T);
-
     // Generar número aleatorio entre 0 y 1
     double random_number = gsl_rng_uniform(r);
-
-    gsl_rng_free(r);
 
     return random_number;
 }
 
-int entero_gsl() 
+int entero_gsl(gsl_rng * r) 
 {
-    const gsl_rng_type * T;
-    gsl_rng * r;
-
-    gsl_rng_env_setup();
-
-    T = gsl_rng_default;
-    r = gsl_rng_alloc (T);
-
     // Generar número aleatorio entero
     int random_int = gsl_rng_uniform_int(r, INT_MAX);
 
-    gsl_rng_free(r);
-
     return random_int;
 }
+
