1fe64f4ec6a0c8608ee02b44ecb3aa76e82cfa4b
ZZAZZ99 <nicolasryzhov@gmail.com>
2024-04-23 13:41:38 +0200

    100

--- obligatorio_2/ising_gsl.c
+++ obligatorio_2/ising_gsl.c
@@ -32,8 +32,8 @@ int main(void)
     Tempe=2.27; //Temperatura de la red
 
     //Dimensión de nuestra red
-    filas = 128; //Filas
-    columnas = 128; //Columnas
+    filas = 100; //Filas
+    columnas = 100; //Columnas
 
     //Abro el archivo donde se guardará la matriz
     FILE *DIPOLE;
