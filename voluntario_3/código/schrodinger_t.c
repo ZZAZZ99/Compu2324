ea89e513fb935e58bb41489bb05709d740599294
ZZAZZ99 <nicolasryzhov@gmail.com>
2024-07-15 00:30:24 +0200

    "error solucionado"

--- voluntario_3/código/schrodinger_t.c
+++ voluntario_3/código/schrodinger_t.c
@@ -5,11 +5,10 @@
 #include <time.h>
 
 #define PI 3.14159265358979323846
-#define barreras 1
+#define barreras 100
 #define width 100
 #define sep 150
 #define N (600+width+(barreras-1)*sep)
-#define M 1000  // Define el número de Gaussianas
 
 void nacimiento(double complex* psi, double ko);
 void radiacion(double complex* gamma, double complex* alpha, double* V, double s);
@@ -17,39 +16,62 @@ void calculoB(double complex* b, double complex* psi, double s);
 void calculobeta(double complex* beta, double complex* gamma, double complex* b);
 double constitucion(double complex* vector);
 double nomeven_dere(double complex* vector);
+void barrelroll(double complex *psi, double complex *psi2);
+double posicion(double complex *vector);
+double cinetica(double complex *D2psi, double complex *psi2);
+double desviacion(double *vector, double media);
+void derivacion(double complex *vector, double complex *derivada);
 
 int main() 
 {
-    double *V, s, ko, nciclos, lambda, norma2, norma, p_dere, p_dere_aux, m_t, t, p, dist, almacen, h;
-    double complex *psi, *alpha, *beta, *b, *Chi, *gamma;
+    double *V, s, ko, nciclos, lambda, norma2, norma, p_dere, p_dere_aux, m_t, t, p, dist, almacen, h, *x, *T, e_x, e_T, mediax, mediaT;
+    double complex *psi, *psi2, *alpha, *beta, *b, *Chi, *gamma, *D2psi;
     int encontrado;
 
-    //printf("N = %d, sep = %d, width = %d\n", N, sep, width);
+    printf("N = %d, sep = %d, width = %d\n", N, sep, width);
 
     //Inicializo el valor de la serie de números aleatorios
     srand(time(NULL));
 
+    //Ficheros
+    //FILE *PELI = fopen("posicion.dat", "w");
+    //FILE *CINE = fopen("cinetica.dat", "w");
+
+    //fprintf(PELI, "x;y\n"); //Cabecera de los ficheros para generar un plot
+    //fprintf(CINE, "x;y\n");
+
     //Asignamos memoria dinámica
     psi = (double complex*)malloc((N+1) * sizeof(double complex));
+    psi2 = (double complex*)malloc((N+1) * sizeof(double complex));
     alpha = (double complex*)malloc(N * sizeof(double complex));
     beta = (double complex*)malloc(N * sizeof(double complex));
     b = (double complex*)malloc(N * sizeof(double complex));
     Chi = (double complex*)malloc(N * sizeof(double complex));
     gamma = (double complex*)malloc(N * sizeof(double complex));
     V = (double*)malloc((N+1) * sizeof(double));
+    D2psi = (double complex *)malloc(sizeof(double complex) * (N + 1));
+    x = (double*)malloc(10*N * sizeof(double));
+    T = (double*)malloc(10*N * sizeof(double));
 
     // Definimos constantes
+    // Los ciclos los escoges tú (máximo N/4)
+
     encontrado = 0;
     p_dere = 0;
     lambda = 0.3;
-    nciclos = N / 5;
+    nciclos = 50; //N / 5;
     ko = 2 * PI * nciclos / N;
     s = 1 / (4.0 * ko * ko);
     dist = sep - width;
     h = 2*ko;
 
+    mediax = 0.0;
+    mediaT = 0.0;
+
     // Inicialización del potencial
-    for (int j = 0; j < (2 * N / 5); j++) 
+    
+    
+    /*for (int j = 0; j < (2 * N / 5); j++) 
     {
         V[j] = 0.0;
     }
@@ -62,6 +84,26 @@ int main()
     for (int j = (3 * N / 5) + 1; j < N + 1; j++) 
     {
         V[j] = 0.0;
+    }*/
+
+    
+    for (int i = 0; i < 300; i++) 
+    {
+        V[i] = 0;
+    }
+    for (int k = 0; k < barreras; k++) 
+    {
+        for (int i = 300 + k * width + k * dist; i < 300 + (k + 1) * width + (k + 1) * dist; i++) 
+        {
+            if (i > (300 + k * width + k * dist) && i < (300 + (k + 1) * width + k * dist)) 
+            {
+                V[i] = lambda * ko * ko;
+            } 
+            else 
+            {
+                V[i] = 0.0;
+            }
+        }
     }
 
     // Inicializa la función psi
@@ -102,14 +144,39 @@ int main()
         if ((p_dere_aux >= p_dere) && (encontrado == 0)) 
         {
             p_dere = p_dere_aux;
+
             t = j;
         } 
         else if ((p_dere - p_dere_aux) > 0.001) 
         {
             encontrado = 1;
         }
+
+        //Cálculo de valor esperado de la posición y energía cinética
+
+        barrelroll(psi, psi2);
+        derivacion(psi, D2psi);
+        derivacion(D2psi, D2psi);
+
+        x[j] = posicion(psi);
+        mediax += x[j];
+
+        T[j] = cinetica(D2psi, psi2);
+        mediaT += T[j];
+
+        //fprintf(PELI, "%d;%f\n", j, x[j]);
+        //fprintf(CINE, "%d;%f\n", j, T[j]);
     }
 
+    mediax = mediax / (N * 10);
+    mediaT = mediaT / (N * 10);
+
+    e_x = desviacion(x, mediax);
+    e_T = desviacion(T, mediaT);
+
+    //printf("El valor esperado de la posición es: %f;%f\n", mediax, e_x);
+    //printf("El valor esperado de la energía cinética es: %f;%f\n", mediaT, e_T);
+    
     p_dere = p_dere / norma;
     
     m_t = 0;
@@ -124,16 +191,23 @@ int main()
         }
     }
     
-    printf("El valor de K es: %f\n", 1.0*m_t/1000.0); //1000 iteraciones
-    printf("El valor de la probabilidad a la derecha es: %f\n", p_dere);
+    //printf("El valor de K es: %f\n", 1.0*m_t/1000.0); //1000 iteraciones
+    //printf("El valor de la probabilidad a la derecha es: %f\n", p_dere);
 
     free(psi);
+    free(psi2);
     free(alpha);
     free(beta);
     free(b);
     free(Chi);
     free(gamma);
     free(V);
+    free(D2psi);
+    free(x);
+    free(T);
+
+    //fclose(PELI);
+    //fclose(CINE);
 
     return 0;
 }
@@ -141,22 +215,16 @@ int main()
 void nacimiento(double complex* psi, double ko) 
 {
     double sigma, centro;
-    double dist = N / (M + 1); // Distancia entre los centros de las Gaussianas
 
     psi[0] = 0.0;
     psi[N] = 0.0;
 
     sigma = N / 20.0;
+    centro = N / 8.0;
 
-    // Inicializamos psi con M Gaussianas
-    for (int m = 0; m < M; m++) 
+    for (int i = 1; i < N; i++) 
     {
-        centro = dist * (m + 1); // Centro de la m-ésima Gaussiana
-
-        for (int i = 1; i < N; i++) 
-        {
-            psi[i] += cexp(1.0 * I * ko * i) * cexp(-1.0 * (i - centro) * (i - centro) / (2.0 * sigma * sigma));
-        }
+        psi[i] = cexp(1.0 * I * ko * i) * cexp(-1.0 * (i - centro) * (i - centro) / (2.0 * sigma * sigma));
     }
 }
 
@@ -219,4 +287,78 @@ double nomeven_dere(double complex* vector)
     }
 
     return suma;
+}
+
+void barrelroll(double complex *psi, double complex *psi2)
+{
+    double preal, pimag;
+
+    for (int i = 0; i < N+1; i++)
+    {
+        preal = creal(psi[i]);
+        pimag = -cimag(psi[i]);
+        psi2[i] = preal + I*pimag;
+    }
+    
+    return ;
+}
+
+double posicion(double complex *vector)
+{
+    double suma, norma;
+
+    suma=0;
+    norma=0;
+
+    for (int i = 0; i < N+1; i++)
+    {
+        suma += i*cabs(vector[i]);
+        norma += cabs(vector[i]);
+    }
+    
+    return suma/norma;
+}
+
+double cinetica(double complex *D2psi, double complex *psi2)
+{
+    double suma=0;
+    double norma=0;
+    for (int i = 0; i < N+1; i++)
+    {
+        suma += -creal(psi2[i]*D2psi[i]);
+        norma += cabs(psi2[i]);
+    }
+
+    return suma/(2*norma); 
+}
+
+double desviacion(double *vector, double media)
+{
+    double suma;
+    suma = 0.0;
+
+    for (int i = 0; i < 10*N; i++)
+    {
+        suma += pow((vector[i] - media),2);
+    }
+
+    return sqrt(suma/N);
+}
+
+void derivacion(double complex *vector, double complex *derivada)
+{
+    for (int i = 0; i < N+1; i++)
+    {
+        if (i==N)
+        {
+            derivada[i] = 0.0;
+        }
+        else
+        {
+            derivada[i] = vector[i+1] - vector[i];
+        }
+        
+    }
+
+    return;
 }
